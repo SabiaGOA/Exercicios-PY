36f60ccf458c453a55a45f850fadc0357ccc1ee1
GetSam <139009948+SabiaGOA@users.noreply.github.com>
2023-09-17 13:33:40 -0300

    Update and rename ex010.py to Conversor  de Moedas.py

--- Exercicios/ex010.py
+++
@@ -1,5 +0,0 @@
-v = float(input('Digite quanto você tem na carteira R$'))
-d = v / 4.86
-e = v / 5.28
-i = v / 0.034
-print (f'Com R${v:.2f} Você conseguira compra US${d:.2f} em Euro será EU${e:.2f} e em Ien Y${i:.2f}')
