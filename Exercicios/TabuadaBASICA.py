a9f677fb1060c29a73b4f4f855bd6805395f24c6
SabiaGOA <zoinhocunha1357@gmail.com>
2023-09-17 12:10:36 -0300

    Create TabuadaBASICA.py
    So Julgue se vc tiver feito um pior :>

---
+++ Exercicios/TabuadaBASICA.py
@@ -0,0 +1,26 @@
+n = int(input('Digite um número que deseja a tabuada'))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print (f'1x{n} = {n1}')
+print (f'2x{n} = {n2}')
+print (f'3x{n} = {n3}')
+print (f'4x{n} = {n4}')
+print (f'5x{n} = {n5}')
+print (f'6x{n} = {n6}')
+print (f'7x{n} = {n7}')
+print (f'8x{n} = {n8}')
+print (f'9x{n} = {n9}')
+print (f'10x{n} = {n10}')
+
+
+
+
+
